25a89061ea4ed32e25945f0a5b22c88edc69403c
Coding Cande <codingcande@gmail.com>
2022-08-12 01:29:05 -0300

    search bar

--- google-clone/src/contexts/ResultContextProvider.jsx
+++ google-clone/src/contexts/ResultContextProvider.jsx
@@ -6,7 +6,7 @@ const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
 export const ResultContextProvider = ({children}) => {
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("JS Mastery");
+  const [searchTerm, setSearchTerm] = useState("Elon Musk");
 
   const getResults = async (type) => {
     setLoading(true);
@@ -15,16 +15,23 @@ export const ResultContextProvider = ({children}) => {
       method: "GET",
       headers: {
         "X-User-Agent": "desktop",
-        "X-Proxy-Location": "EU",
-        "X-RapidAPI-Key": "498f5f4174msh1373f22c0e99a9dp1c26ebjsn09d61aafaf94",
+        "X-RapidAPI-Key": import.meta.env.VITE_API_KEY,
         "X-RapidAPI-Host": "google-search3.p.rapidapi.com",
       },
     });
     const data = await response.json();
 
-    setResults(data);
-    setLoading(false);
+    if(type.includes("/news")) {
+      setResults (data.entries);
+    } else if (type.includes("/images")) {
+      setResults (data.image_results);
+    } else {
+      setResults (data.results);
+    }
+
+    setLoading(false)
   };
+
   return (
     <ResultContext.Provider
       value={{ getResults, results, searchTerm, setSearchTerm, loading }}>
